0408fbef213ca490831cc0550f497bb600f4bdf6
pathYu <1974675011@qq.com>
2021-09-07 23:23:24 +0800

    解决变量冲突

--- src/components/Menu/SubMenu.tsx
+++ src/components/Menu/SubMenu.tsx
@@ -10,39 +10,45 @@ import { MenuContext } from './Menu';
 import { MenuItemProps } from './MenuItem';
 
 
-export interface SubMenu{
+export interface SubMenuProps{
   index?: string,
   // x下拉菜单的名字
   title: string,
   className?:string
 }
 
-export const SubMenu: FC<SubMenu> = ({ index,title,children,className}) => {
+export const SubMenu: FC<SubMenuProps> = ({ index, title, children, className }) => {
   const context = useContext(MenuContext);
   const openedSubMenus = context.defaultOpenSubMenus as Array<string>;
-  const isOpen = (index && context.mode === 'vertical') ? openedSubMenus.includes(index) : false;
+  const isOpen =
+    index && context.mode === 'vertical'
+      ? openedSubMenus.includes(index)
+      : false;
   const [menuOpen, setOpen] = useState(isOpen);
-  const classes = classNames('menu-item','submenu-item',className, {
-    'is-active':context.index === index
-  })
+  const classes = classNames('menu-item', 'submenu-item', className, {
+    'is-active': context.index === index,
+  });
   const subMenuClass = classNames('simple-submenu', {
     'menu-opened': menuOpen,
   });
   const handleCLick = (e: MouseEvent) => {
-    e.preventDefault()
-    setOpen(!menuOpen)
-  }
+    e.preventDefault();
+    setOpen(!menuOpen);
+  };
   let timer: any;
-  const handleMouse = (e: MouseEvent, toggle:boolean) => {
-    clearTimeout(timer)
-    e.preventDefault()
+  const handleMouse = (e: MouseEvent, toggle: boolean) => {
+    clearTimeout(timer);
+    e.preventDefault();
     timer = setTimeout(() => {
-      setOpen(toggle)
-    })
-  }
-  const clickEvents = context.mode === 'vertical' ? {
-    onClick: handleCLick
-  } : {};
+      setOpen(toggle);
+    });
+  };
+  const clickEvents =
+    context.mode === 'vertical'
+      ? {
+          onClick: handleCLick,
+        }
+      : {};
   const hoverEvents =
     context.mode !== 'vertical'
       ? {
@@ -50,22 +56,22 @@ export const SubMenu: FC<SubMenu> = ({ index,title,children,className}) => {
           onMouseLeave: (e: MouseEvent) => handleMouse(e, false),
         }
       : {};
-  
+
   const renderChildren = () => {
     const childrenComponent = Children.map(children, (child, i) => {
       const childElement = child as FunctionComponentElement<MenuItemProps>;
       if (childElement.type.displayName === 'MenuItem') {
         return cloneElement(childElement, {
-          index:`${index}-${i}`
+          index: `${index}-${i}`,
         });
       } else {
         console.error(
           'Warning: SubMenu has a child which is not a MenuItem component'
         );
       }
-    })
+    });
     return <ul className={subMenuClass}>{childrenComponent}</ul>;
-  }
+  };
   return (
     <li key={index} className={classes} {...hoverEvents}>
       <div className="submenu-title" {...clickEvents}>
@@ -74,6 +80,6 @@ export const SubMenu: FC<SubMenu> = ({ index,title,children,className}) => {
       {renderChildren()}
     </li>
   );
-}
+};
 SubMenu.displayName = 'SubMenu';
 export default SubMenu
