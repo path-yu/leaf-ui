74a37e1878f4921b56d2b18dbd200cce080ec584
pathYu <1974675011@qq.com>
2021-09-08 22:50:51 +0800

    完善Menu测试用例代码

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,6 +1,12 @@
-import { fireEvent, render, RenderResult } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  RenderResult,
+  waitFor
+} from '@testing-library/react';
 import Menu, { MenuProps } from './Menu';
 import { MenuItem } from './MenuItem';
+import SubMenu from './SubMenu';
 
 const testProps: MenuProps = {
   defaultIndex: '0',
@@ -10,8 +16,21 @@ const testProps: MenuProps = {
 const testVerticalProps: MenuProps = {
   defaultIndex: '0',
   mode: 'vertical',
+  defaultOpenSubMenus: ['4'],
 };
-
+const createStyleFile = () => {
+  const cssFile: string = `
+   .simple-submenu {
+      display: none;
+    }
+    .menu-opened {
+      display:block;
+    } 
+  `;
+  const style = document.createElement('style');
+  style.innerHTML = cssFile;
+  return style;
+};
 const generateMenu = (props: MenuProps) => {
   return (
     <Menu {...props}>
@@ -20,6 +39,12 @@ const generateMenu = (props: MenuProps) => {
       <MenuItem index="2" disabled>
         disabled
       </MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>drop1</MenuItem>
+      </SubMenu>
+      <SubMenu title="opened">
+        <MenuItem>opened1</MenuItem>
+      </SubMenu>
     </Menu>
   );
 };
@@ -31,6 +56,7 @@ let wrapper: RenderResult,
 describe('test Menu and MenuItem component in default(horizontal) mode', () => {
   beforeEach(() => {
     wrapper = render(generateMenu(testProps));
+    wrapper.container.append(createStyleFile());
     menuElement = wrapper.getByTestId('test-menu');
     activeElement = wrapper.getByText('active');
     disabledElement = wrapper.getByText('disabled');
@@ -38,7 +64,7 @@ describe('test Menu and MenuItem component in default(horizontal) mode', () => {
   it('should render correct Menu and MenuItem based on default props', () => {
     expect(menuElement).toBeInTheDocument();
     expect(menuElement).toHaveClass('simple-menu test');
-    expect(menuElement.children.length).toEqual(3);
+    expect(menuElement.children.length).toEqual(5);
     expect(activeElement).toHaveClass('menu-item is-active');
     expect(disabledElement).toHaveClass('menu-item is-disabled');
   });
@@ -51,13 +77,43 @@ describe('test Menu and MenuItem component in default(horizontal) mode', () => {
     expect(disabledElement).not.toHaveClass('is-active');
     expect(testProps.onSelect).not.toHaveBeenCalledWith('2');
   });
+  it('should show dropdown items when hover on SubMenu', async () => {
+    expect(wrapper.queryByText('drop1')).not.toBeVisible();
+    const dropdownElement = wrapper.getByText('dropdown');
+    fireEvent.mouseEnter(dropdownElement);
+    await waitFor(
+      () => {
+        expect(wrapper.queryByText('drop1')).toBeVisible();
+      },
+      { timeout: 300 }
+    );
+    fireEvent.click(wrapper.getByText('drop1'));
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+    fireEvent.mouseLeave(dropdownElement);
+    await waitFor(
+      () => {
+        expect(wrapper.queryByText('drop1')).not.toBeVisible();
+      },
+      { timeout: 300 }
+    );
+  });
 });
 describe('test menu and MenuItem component in vertical mode', () => {
   beforeEach(() => {
     wrapper2 = render(generateMenu(testVerticalProps));
+    wrapper2.container.append(createStyleFile());
   });
   it('should render vertical mode when mode is set to vertical', () => {
     const menuElement = wrapper2.getByTestId('test-menu');
     expect(menuElement).toHaveClass('menu-vertical');
   });
+  it('should show dropdown items when click on subMenu for vertical mode', () => {
+    const dropDownItem = wrapper2.getByText('drop1');
+    expect(dropDownItem).not.toBeVisible();
+    fireEvent.click(wrapper2.getByText('dropdown'));
+    expect(dropDownItem).toBeVisible();
+  });
+   it('should show subMenu dropdown when defaultOpenSubMenus contains SubMenu index', () => {
+     expect(wrapper2.queryByText('opened1')).toBeVisible();
+   });
 });
