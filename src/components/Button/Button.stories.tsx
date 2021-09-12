2c0a3574267e50c87968543dac986adaecbc12dd
pathYu <1974675011@qq.com>
2021-09-12 23:35:44 +0800

    更新Button文档

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,9 +1,9 @@
 // Button.stories.ts | Button.stories.tsx
 
+import { action } from '@storybook/addon-actions';
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
 import Button, { ButtonProps } from './Button';
-
 
 
 export default {
@@ -12,8 +12,21 @@ export default {
 } as Meta;
 
 //👇 We create a “template” of how args map to rendering
-const Template: Story<ButtonProps> = (args) => <Button {...args} >简单的Button </Button>;
+const Template: Story<ButtonProps> = (args) => (
+  <Button onClick={action('clicked')} {...args}>
+    简单的Button
+  </Button>
+);
+export const buttonWithSize = () => (
+  <>
+    <Button size="lg"> large button </Button>
+    <Button size="sm"> small button </Button>
+  </>
+);
 
-export const Primary = Template.bind({});
-Primary.storyName = 'I am the primary';
+export const Default = Template.bind({});
+
+Default.args = {
+};
+Default.storyName = 'Button';
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,20 +9,32 @@ import {
 export type ButtonType = 'primary' | 'default' | 'danger' | 'link';
 export type ButtonSize = 'lg' | 'sm';
 export type ButtonShare = 'circle' | 'round';
-interface BaseButtonProps {
-  className?: string;
-  disabled?: boolean; // 设置disable的禁用
-  size?: ButtonSize; // 设置Button的尺寸
-  btnType?: ButtonType; // 设置Button的类型
-  children: ReactNode;
-  href?: string;
-  share?: ButtonShare;
-}
+ interface BaseButtonProps {
+   className?: string;
+   /**是否禁用 Button */
+   disabled?: boolean;
+   /**设置 Button 的大小 */
+   size?: ButtonSize;
+   /**设置 Button 的类型 */
+   btnType?: ButtonType;
+   children: ReactNode;
+   /**当按钮为link类型时的href链接地址 */
+   href?: string;
+   /**button的形状 */
+   share?: ButtonShare;
+ }
 // * 将原生元素button和a标签的props属性和Button自定义的props属性类型进行联合
 type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>;
 type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>;
 // ! 最终Button组件的props类型文件
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
+/**
+ * 页面中最常用的的按钮元素，适合于完成特定的交互，支持 HTML button 和 a 链接 的所有属性
+ * ### 引用方法
+ * ```js
+ * import { Button } from 'simple-element'
+ * ```
+ */
 const Button: FC<ButtonProps> = (props) => {
   const { className, btnType, disabled, size, children, href,share, ...resetProps } =
     props;
