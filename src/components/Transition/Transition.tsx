fed5026d84e3efed1606afae270c9c786824fbe5
pathYu <1974675011@qq.com>
2021-09-12 21:31:16 +0800

    封装transition组件

---
+++ src/components/Transition/Transition.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { CSSTransitionProps } from 'react-transition-group/CSSTransition';
+type AnimationName =
+  | 'zoom-in-top'
+  | 'zoom-in-left'
+  | 'zoom-in-bottom'
+  | 'zoom-in-right';
+
+type TransitionProps = CSSTransitionProps & {
+  animation?: AnimationName;
+  wrapper?: boolean;
+};
+
+
+
+const Transition: FC<TransitionProps> = (props) => {
+  const {
+    children,
+    classNames,
+    animation,
+    wrapper,
+    ...restProps
+  } = props;
+  return (
+    <CSSTransition
+      classNames={classNames ? classNames : animation}
+      {...restProps}
+    >
+      {wrapper ? <div>{children}</div> : children}
+    </CSSTransition>
+  );
+};
+
+export default Transition
