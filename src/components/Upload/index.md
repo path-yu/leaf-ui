df220e0e57485ed4f36936950508339db4309c64
pathYu <1974675011@qq.com>
2021-09-17 12:49:58 +0800

    完成Upload上传组件

--- src/components/Upload/index.md
+++ src/components/Upload/index.md
@@ -1,35 +1,85 @@
 ## Upload
 
+### 简单演示
+
 ```tsx
 import React from 'react';
-import { Upload } from 'react-element';
-const checkFileSize = (file: File) => {
-  if (Math.round(file.size / 1024) > 50) {
-    alert('file too big');
-    return false;
-  }
-  return true;
+import { Upload, Button, Icon } from 'react-element';
+const action = (status) => {
+  return () => {
+    console.log(status);
+  };
 };
-const createNewFile = (file: File) => {
-  const newFile = new File([file], 'new_name', { type: file.type });
-  return Promise.resolve(newFile);
+const simpleUpload = () => (
+  <Upload
+    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+    onChange={action('changed')}
+    onSuccess={action('success')}
+    onProgress={action('progress')}
+    onRemove={action('removed')}
+  >
+    <Button size="lg" btnType="primary">
+      <Icon icon="upload" /> 点击上传{' '}
+    </Button>
+  </Upload>
+);
+export default simpleUpload;
+```
+
+### 上传前检查大小
+
+```tsx
+import React from 'react';
+import { Upload, Button, Icon } from 'react-element';
+const checkUpload = () => {
+  const checkFileSize = (file: File) => {
+    if (Math.round(file.size / 1024) > 50) {
+      alert('file too big');
+      return false;
+    }
+    return true;
+  };
+  return (
+    <Upload
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      onChange={() => {
+        console.log('checked');
+      }}
+      beforeUpload={checkFileSize}
+    >
+      <Button size="lg" btnType="primary">
+        <Icon icon="upload" /> 不能传大于50Kb！{' '}
+      </Button>
+    </Upload>
+  );
 };
-export default () => (
+export default checkUpload;
+```
+
+### 拖动上传
+
+```tsx
+import React from 'react';
+import { Upload, Button, Icon } from 'react-element';
+const dragUpload = () => (
   <Upload
     action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
     onChange={() => {
-      console.log('changeEd');
+      console.log('checked');
     }}
-    onProgress={() => {
-      console.log('onProgress');
+    onRemove={() => {
+      console.log('removed');
     }}
-    onError={() => {
-      console.log('error');
-    }}
-    onSuccess={() => {
-      console.log('onSuccess');
-    }}
-    beforeUpload={createNewFile}
-  ></Upload>
+    name="fileName"
+    multiple
+    drag
+  >
+    <Icon icon="upload" size="5x" theme="secondary" />
+    <br />
+    <p>点击或者拖动到此区域进行上传</p>
+  </Upload>
 );
+export default dragUpload;
 ```
+
+<API src="./upload.tsx">
--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -1,27 +1,100 @@
 import axios from 'axios';
-import React, { FC, useRef } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import { ChangeEvent } from 'react-router/node_modules/@types/react';
-import Button from '../Button/button';
+import Dragger from './drag';
+import UploadList from './upload-list';
+export type UploadFileStatus = 'ready' | 'uploading' | 'success' | 'error';
+export interface UploadFile {
+  uid: string;
+  name: string;
+  size: number;
+  status?: UploadFileStatus;
+  percent: number;
+  raw?: File;
+  response?: any;
+  error?: any;
+}
+
 export interface UploadProps {
+  /**必选参数, 上传的地址 */
   action: string;
+  /**上传的文件列表,*/
+  defaultFileList?: UploadFile[];
+  /** 上传文件前的钩子参数为上传的文件，若返回 false 或者 Promise 则停止上传*/
   beforeUpload?: (file: File) => boolean | Promise<File>;
-  onProgress?: (percentage: number, file: File) => void;
-  onSuccess?: (data: any, file: File) => void;
-  onError?: (err: any, file: File) => void;
+  /**文件上传时的钩子 */
+  onProgress?: (percentage: number, file: UploadFile) => void;
+  /**文件上传成功时的钩子 */
+  onSuccess?: (data: any, file: UploadFile) => void;
+  /**文件上传失败时的钩子 */
+  onError?: (err: any, file: UploadFile) => void;
   /**文件状态改变时的钩子，上传成功或者失败时都会被调用	 */
-  onChange?: (file: File) => void;
+  onChange?: (file: UploadFile) => void;
+  /**文件列表移除文件时的钩子 */
+  onRemove?: (file: UploadFile) => void;
+  /** 设置上传的请求头部' */
+  headers?: { [key: string]: any };
+  /**上传文件的字段名 */
+  name?: string;
+  /** 上传是附带的参数 */
+  data?: { [key: string]: any };
+  /**支持发送 cookie 凭证信息 */
+  withCredentials?: boolean;
+  /**可选参数, 接受上传的文件类型 */
+  accept?: string;
+  /**是否支持多选文件 */
+  multiple?: boolean;
+  /**是否支持拖拽上传 */
+  drag?: boolean;
 }
 const UpLoad: FC<UploadProps> = (props) => {
-  const { action, onError, onProgress, onSuccess, beforeUpload, onChange } =
-    props;
+  const {
+    action,
+    onError,
+    onProgress,
+    onSuccess,
+    beforeUpload,
+    onChange,
+    onRemove,
+    name,
+    headers,
+    data,
+    withCredentials,
+    accept,
+    multiple,
+    children,
+    drag,
+    defaultFileList,
+  } = props;
   const fileInputRef = useRef<HTMLInputElement>(null);
-
+  const [fileList, setFileList] = useState<UploadFile[]>(defaultFileList || []);
+  // 触发input的Click事件拉去选择文件的对话框
   const handleClick = () => {
-    console.log('click');
-
     if (fileInputRef.current) {
       fileInputRef.current.click();
     }
+  };
+  const handleRemove = (file: UploadFile) => {
+    setFileList((prevList) => {
+      return prevList.filter((item) => item.uid !== file.uid);
+    });
+    if (onRemove) {
+      onRemove(file);
+    }
+  };
+  const updateFileList = (
+    updateFile: UploadFile,
+    updateObj: Partial<UploadFile>,
+  ) => {
+    setFileList((prevList) => {
+      return prevList.map((file) => {
+        if (file.uid === updateFile.uid) {
+          return { ...file, ...updateObj };
+        } else {
+          return file;
+        }
+      });
+    });
   };
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -49,49 +122,91 @@ const UpLoad: FC<UploadProps> = (props) => {
     });
   };
   const post = (file: File) => {
+    //创建UploadFiled对象 更新fileList
+    let _file: UploadFile = {
+      uid: Date.now() + 'upload-file',
+      status: 'ready',
+      name: file.name,
+      size: file.size,
+      percent: 0,
+      raw: file,
+    };
+    setFileList((prevList) => {
+      return [_file, ...prevList];
+    });
     const formData = new FormData();
-    formData.append(file.name, file);
+    formData.append(name || 'file', file);
+    if (data) {
+      Object.keys(data).forEach((key) => {
+        formData.append(key, data[key]);
+      });
+    }
     axios
       .post(action, formData, {
         headers: {
+          ...headers,
           'Content-Type': 'multipart/form-data',
         },
+        withCredentials,
         onUploadProgress: (e) => {
           let percentage = Math.round((e.loaded * 100) / e.total) || 0;
-          console.log(percentage);
-
           if (percentage < 100) {
-            onProgress && onProgress(percentage, file);
+            updateFileList(_file, { percent: percentage, status: 'uploading' });
+            onProgress && onProgress(percentage, _file);
           }
         },
       })
       .then((resp) => {
-        console.log(resp);
-        if (onSuccess) {
-          onSuccess(resp.data, file);
-        }
-        onChange && onChange(file);
+        updateFileList(_file, { status: 'success', response: resp.data });
+        _file.status = 'success';
+        _file.response = resp.data;
+        onSuccess && onSuccess(resp.data, _file);
+        onChange && onChange(_file);
       })
       .catch((err) => {
-        if (onError) {
-          onError(err, file);
-        }
-        onChange && onChange(file);
+        updateFileList(_file, { status: 'error', error: err });
+        _file.status = 'error';
+        _file.error = err;
+        onError && onError(err, _file);
+        onChange && onChange(_file);
       });
   };
   return (
     <div className="simple-upload-component">
-      <Button onClick={handleClick} btnType="primary">
-        Upload File
-      </Button>
-      <input
-        className="simple-file-input"
-        style={{ display: 'none' }}
-        onChange={handleFileChange}
-        type="file"
-        ref={fileInputRef}
-      />
+      <div
+        className="simple-upload-input"
+        style={{ display: 'inline-block' }}
+        onClick={handleClick}
+      >
+        {drag ? (
+          <Dragger
+            onFile={(files) => {
+              uploadFiles(files);
+            }}
+          >
+            {children}
+          </Dragger>
+        ) : (
+          children
+        )}
+        <input
+          className="simple-file-input"
+          style={{ display: 'none' }}
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          type="file"
+          accept={accept}
+          multiple={multiple}
+        />
+      </div>
+      <UploadList fileList={fileList} onRemove={handleRemove} />
     </div>
   );
+};
+UpLoad.defaultProps = {
+  name: 'file',
+  drag: false,
+  withCredentials: false,
+  multiple: false,
 };
 export default UpLoad;
